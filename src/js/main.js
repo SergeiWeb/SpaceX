df3ff7e5dab4c99b12f83fa68d91f779cc7177c9
Sergei <sergokondrashevskij@ukr.net>
2020-08-18 17:43:23 +0300

    Created a mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,13 @@ import './modules/videobg'
 import './plugins/rellax'
 
 const burgerBtn = document.querySelector('#burger')
-const mainLists = document.querySelectorAll('.header .main-list')
+const mainNav = document.querySelector('.main-nav')
 
 burgerBtn.addEventListener('click', function () {
 	this.classList.toggle('burger-btn--active')
-	document.querySelector('.main-nav').classList.toggle('main-nav--active')
+	mainNav.classList.toggle('main-nav--active')
 
-	mainLists.forEach(item => {
-		if (this.classList.contains('burger-btn--active')) {
-			item.style.display = 'flex'
-		} else {
-			item.style.display = 'none'
-		}
-	})
+	this.classList.contains('burger-btn--active')
+		? (document.body.style.overflowY = 'hidden')
+		: (document.body.style.overflowY = '')
 })
